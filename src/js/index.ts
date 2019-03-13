e7bc76a9eeb48a3f9d08bfb9abc644eb1053342b
DanMoll <dani948d@edu.easj.dk>
2019-03-13 09:31:21 +0100

    Made input field + button; begun work on function

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,8 @@
 //Her skal du skrive din typescriptkode
+document.getElementById('AddVare').addEventListener("click",InputVare);
+let inputElement = document.getElementById('vareName');
+function InputVare(){
+    console.log('Button was clicked, added:');
+    console.log(inputElement.nodeValue);
+    let 
+}
